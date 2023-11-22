2604cee8080ee2a7cabdb7daf9e4fa54041a0773
siciovas <rokas.siciovas@inbox.lt>
2023-11-22 18:23:11 +0200

    init roulette screen project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="container">
+        <h2 class="text-center mt-3">Aardvark - "Roulette Screen" game</h2>
+        <label class="">API's URL</label>
+        <input type="text" class="form-control" />
+    </div>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import './assets/base.css'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap"
+
+createApp(App).mount("#app");
